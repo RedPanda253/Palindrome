5c0b165819ee04b7e0a59cdacc4c0fb091724f88
RedPanda253 <pandagamer2531@gmail.com>
2023-01-06 08:44:39 -0700

    Setting up inital code

---
+++ Palindrome/Palindrome/Palindrome.cpp
@@ -0,0 +1,19 @@
+/*Palindrome.cpp
+ *Isaiah Hocking
+ *1-6-23
+ *Purpose
+ *		Create a program that allows a user to imput a sting and check to see if it is a palindrome
+ */
+
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+
+using namespace std;
+
+bool check(string = "nothing");
+string reduce(string = "nothing");
+
+int main() {
+
+}

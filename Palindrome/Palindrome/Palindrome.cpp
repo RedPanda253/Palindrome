d375e68a67fd5e8ca4117bd2301ff9dd314f82d8
RedPanda253 <pandagamer2531@gmail.com>
2023-01-10 11:45:16 -0700

    adding comments

--- Palindrome/Palindrome/Palindrome.cpp
+++ Palindrome/Palindrome/Palindrome.cpp
@@ -10,36 +10,34 @@
 #include<string>
 #include<vector>
 
-
 using namespace std;
-
+//prototyping both functions to allow for them to be defined after the main function.
 bool check(string = "nothing");
 string reduce(string = "nothing");
-
 
 int main() {
 	string repeat,
 		input,
 		reduced_string;
+	//opening text
 	cout << "Welcome to the palindrome tester.\n"
 		<< "When you are done just press enter when inputting to exit\n";
 	do {
-		
 			cout << "Please enter a word or phrase to test:\n";
 		getline(cin, input);
 		cin.clear();
+		//checking for an empty string to exit the program
 		if (input == "") {
 			break;
 		}
-		//cout << reduce(input);
+		//cout << reduce(input);to check for palindromes
+		//using check and reduce 
 		if (check(reduce(input))) {
 			cout << input << " Is a palindrome\n";
 		}
 		else {
 			cout << input << " Is not a palindrome\n";
 		}
-
-
 	} while (true);
 
 
@@ -47,16 +45,20 @@ int main() {
 
 bool check(string input) {
 	bool retVal = false;
+	//making two variables to store the values of two letters opposite from eachother in the word to compare them
 	int check1 = 0, check2 = input.size()-1;
 	while(check1 <= check2) {
 		if (check1 == check2) {
 			retVal = true;
 			break;
 		}
+		//checking if check 2 is one more than check 1 and checking if the letters at both spots are equal
+		//to work with words that have an even number of letters
 		else if (check2 - 1 == check1 && input[check1] == input[check2]) {
 			retVal = true;
 			break;
 		}
+		//checking if each letter are the same on either side of the word.
 		if (input[check1] == input[check2]) {
 			check1++;
 			check2--;
@@ -73,11 +75,13 @@ string reduce(string input) {
 	string reduceVal = input;
 	string retVal;
 	int length = 0;
+	//creating a vector to store each character in the string and making all of the letters lowercase
 	vector <char> arr;
 	for (int x = 0; x < reduceVal.size(); x++) {
 		arr.push_back(tolower(reduceVal[x]));
 	}
-	char num[] = { '0','1','2','3','4','5','6','7','8','9' };
+	//using the ascii values for the letters and numbers to only include them in the return value
+	//to have the reduces word be only lowercase numbers and letters
 	for (int i = 0; i < arr.size(); i++) {
 		if ((arr[i] >= 97 && arr[i] <= 122)or(arr[i] >= 48 && arr[i] <= 57)) {
 			retVal += arr[i];
